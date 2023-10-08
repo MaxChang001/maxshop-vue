216a708f0f9cc27c5a50f371e066ce3deeba73cf
MaxChang001 <woodenhilled@gmail.com>
2023-10-08 17:58:50 +1300

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>这就是一个demo</h1>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
